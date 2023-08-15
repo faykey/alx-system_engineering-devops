f58fd89b07e401bb5eb9342900b136e74d688c89
faykey <fikayomicheal4@gmail.com>
2023-08-15 03:18:33 -0700

    edit

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-# Kills a process named 'killmenow" using Puppet
+# Kills a process named 'killmenow' using Puppet
+
 exec { 'killmenow':
-  command => 'usr/bin/pkill killmenow'
+  command => '/usr/bin/pkill killmenow'
 }
